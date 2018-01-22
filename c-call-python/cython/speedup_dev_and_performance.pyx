9bae3231798f90e37faa8f7cb7b8e2ee1609012a
Peiyu Liao <u3512485@connect.hku.hk>
2018-01-22 11:02:51 +0800

    Add def, cdef, cpdef examples for cython

--- c-call-python/cython/speedup_dev_and_performance.pyx
+++ c-call-python/cython/speedup_dev_and_performance.pyx
@@ -1,5 +1,11 @@
-cdef public complex_and_slow_calc(x, a, b):
-    return a * x + b
+import math 
 
-def complex_and_slow_calc_2(x, a, b):
-    return a * x + b
+
+def complex_and_slow_calc_p(x, a, b):
+    return round(math.sqrt(a * x + b))
+
+cdef public int complex_and_slow_calc_c(int x, int a, int b):
+    return round(math.sqrt(a * x + b))
+
+cpdef public int complex_and_slow_calc_cp(int x, int a, int b):
+    return round(math.sqrt(a * x + b))
