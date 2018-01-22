818530e1ccf54f0d0f0c52ae8e4719d10f53bcc6
Peiyu Liao <u3512485@connect.hku.hk>
2018-01-22 11:02:30 +0800

    Add init module for cython

--- c-call-python/cython/main.c
+++ c-call-python/cython/main.c
@@ -1,32 +1,14 @@
 #include <Python.h>
 #include "speedup_dev_and_performance.h"
 
-// Wrapper method for python function 
-int _complex_and_slow_calc(int x, int a, int b) {
-    PyObject *pRes;
-    int res;
-
-    // Call function with args
-    pRes = complex_and_slow_calc(PyLong_FromLong(x), PyLong_FromLong(a), PyLong_FromLong(b));
-
-    // If no failure, parse the return value into standard c types
-    if (pRes == NULL)
-        return -1;
-    res = PyLong_AsLong(pRes);
-
-    // Decrement reference counts to avoid memory leak
-    Py_DECREF(pRes);
-
-    return res;
-}
-
-// Main
 int main() {
-    // Init Python interpreter
+    // Init module & Python interpreter
+    PyImport_AppendInittab("speedup_dev_and_performance", PyInit_speedup_dev_and_performance);
     Py_Initialize();
+    PyImport_ImportModule("speedup_dev_and_performance");
     
-    // Init module & call Python function
-    printf("%d\n", _complex_and_slow_calc(3, 2, 4));
+    // Call Python function
+    printf("%d\n", complex_and_slow_calc_c(3, 2, 4));
 
     // Frees memory allocated by Python interpreter 
     Py_Finalize();
